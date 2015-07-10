6f3d3813a66f51c793ceee5f8a96e84bfc7041fd
Arvind Narayanan <naray111@umn.edu>
2015-07-10 10:53:16 +0200

    Added data for heat map - look at columns fixed node coordinates (ssid_loc_x, ssid_loc_y)

--- read_db/fp.py
+++ read_db/fp.py
@@ -1,4 +1,22 @@
-def fixed_point(ssid):
-	x = 5500 - 600 * (ord(ssid[-2]) - 98)
+#!/usr/bin/python
+# I ran this script on my local machine, it requries PANDAS library! -Arvind
+
+import pandas as pd
+
+def get_ssid_x(ssid):
+	x = 5500 - 600 * (ord(ssid[-2].lower()) - 98)
+	return x
+
+def get_ssid_y(ssid):
 	y = 200 + 360 * (int(ssid[-1]) - 1)
-	return (x, y)
+	return y
+
+
+df = pd.read_csv('data/rssi_values.csv')
+
+df['ssid_loc_x'] = df.apply(lambda x: get_ssid_x(x['ssid']), axis=1)
+df['ssid_loc_y'] = df.apply(lambda x: get_ssid_y(x['ssid']), axis=1)
+
+del df['s_id']
+
+df.to_csv('for_heat_map.csv', index=False)
